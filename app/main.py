9ff5137c3e148ffd32c9ecf9b470ec665f052449
bashyrov <bashyroov@gmail.com>
2024-10-09 12:11:34 +0200

    second commit

--- app/main.py
+++ app/main.py
@@ -1,62 +1,19 @@
 def format_linter_error(error: dict) -> dict:
     return {"line": error["line_number"], "column": error["column_number"], "message": error["text"], "name": error["code"], "source": "flake8"}
-#     "column": 80,
-#     "message": "line too long (99 > 79 characters)",
-#     "name": "E501",
-#     "source": "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return [{"line": error["line_number"], "column": error["column_number"], "message": error["text"], "name": error["code"], "source": "flake8"} for error in errors]
-    pass
+    return {"errors": [{"line": error["line_number"], "column": error["column_number"], "message": error["text"], "name": error["code"], "source": "flake8"} for error in errors], "path": file_path, "status": "failed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [{"errors": [{"line": error["line_number"], "column": error["column_number"], "message": error["text"], "name": error["code"], "source": "flake8"} for error in errors], "path": path, "status": "failed" if errors else "passed"} for path, errors in linter_report.items()]
 
 
-# errors = [
-#     {
-#         "code": "E501",
-#         "filename": "./source_code_2.py",
-#         "line_number": 18,
-#         "column_number": 80,
-#          "text": "line too long (99 > 79 characters)",
-#         "physical_line": '    return f"I like to filter, rounding, doubling, '
-#         "store and decorate numbers: {', '.join(items)}!\"",
-#     },
-#     {
-#         "code": "W292",
-#         "filename": "./source_code_2.py",
-#         "line_number": 18,
-#         "column_number": 100,
-#         "text": "no newline at end of file",
-#         "physical_line": '    return f"I like to filter, rounding, doubling, '
-#         "store and decorate numbers: {', '.join(items)}!\"",
-#     },
-# ]
-#
-# print(format_single_linter_file(file_path="./source_code_2.py", errors=errors))
-# # The output will be:
-# {
-#     "errors":
-#         [
-#             {
-#                 "line": 18,
-#                 "column": 80,
-#                 "message": "line too long (99 > 79 characters)",
-#                 "name": "E501",
-#                 "source": "flake8"
-#             },
-#             {
-#                 "line": 18,
-#                 "column": 100,
-#                 "message": "no newline at end of file",
-#                 "name": "W292",
-#                 "source": "flake8"
-#             }
-#         ],
-#     "path": "./source_code_2.py",
-#     "status": "failed"
-# }
+
+
+
+
+
+
+

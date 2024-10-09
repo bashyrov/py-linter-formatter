5a68434bb7f4972af91f482defed6f022adc17a0
bashyrov <bashyroov@gmail.com>
2024-10-09 12:24:43 +0200

    fourth  commit

--- app/main.py
+++ app/main.py
@@ -8,9 +8,3 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [{"errors": [{"line": error["line_number"], "column": error["column_number"], "message": error["text"], "name": error["code"], "source": "flake8"} for error in errors], "path": path, "status": "failed" if errors else "passed"} for path, errors in linter_report.items()]
-
-
-
-
-
-
